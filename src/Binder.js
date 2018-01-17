118bf4cc10aa03a619255f9ef5a3cf8247797730
Sky Morey <SMorey@degdigital.com>
2018-01-17 09:48:55 -0600

    updates

--- src/Binder.js
+++ src/Binder.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-function handleBinder(parentProps, props, opt) {
+function handleBinder(parentProps, props, opts) {
   let newProps = {};
   ['error', 'label', 'value', 'onBlur'].forEach(method => {
     let binder = parentProps[method];
     if (!binder) return;
-    if (typeof binder[`${method}For`] === 'function') newProps[method] = binder[`${method}For`](props.id, opt);
-    else if (typeof binder[`${method}Props`] === 'function') binder[`${method}Props`](props, params, newProps);
+    if (typeof binder[`${method}Props`] === 'function') binder[`${method}Props`](props, params, newProps);
+    else if (props[method] === undefined && typeof binder[`${method}For`] === 'function') newProps[method] = binder[`${method}For`](props.id, opts);
   });
   return newProps;
 }
 
-function deepClone(parentProps, children, opt) {
+function deepClone(parentProps, children, opts) {
   return React.Children.map(children, child => {
     if (!child || !child.props) return child;
-    let childProps = React.isValidElement(child) && child.props.id ? handleBinder(parentProps, child.props, opt) : {};
-    childProps.children = deepClone(parentProps, child.props.children, opt);
+    let childProps = React.isValidElement(child) && child.props.id ? handleBinder(parentProps, child.props, opts) : {};
+    childProps.children = deepClone(parentProps, child.props.children, opts);
     return React.cloneElement(child, childProps);
   });
 }
 
 export default class Binder extends Component {
   render() {
-    let opt = this.props.opt;
-    return deepClone(this.props, this.props.children, opt);
+    let opts = this.props.opts;
+    return deepClone(this.props, this.props.children, opts);
   }
 }
 
@@ -32,5 +32,5 @@ Binder.propTypes = {
   error: PropTypes.object,
   label: PropTypes.object,
   onBlur: PropTypes.object,
-  opt: PropTypes.object,
+  opts: PropTypes.object,
 };
